634d667bca59d1570985f9d2b0ba072fd97eb193
trantuanduong2011vn-droid <trantuanduong2011vn@gmail.com>
2025-11-07 20:57:53 +0700

    Delete lib directory

--- lib/ESP32S3-AudioI2S/Audio.cpp
+++
@@ -1,4 +0,0 @@
-#include "Audio.h"
-void Audio::begin() { Serial.println("Audio started"); }
-void Audio::play(const char* url) { Serial.printf("Playing: %s\n", url); }
-void Audio::stop() { Serial.println("Audio stopped"); }
